3eeb587b5bc5dd0c24f68729927f007d5125cb20
Medha Gupta <medha16032@iitd.ac.in>
2016-10-21 18:34:46 +0530

    Intkajdff

---
+++ app/src/main/java/com/example/medhagupta/navigationdrawer/FixturesFragment.java
@@ -0,0 +1,26 @@
+package com.example.medhagupta.navigationdrawer;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anupamchugh on 10/12/15.
+ */
+public class FixturesFragment extends Dialog implements android.view.View.OnClickListner{
+
+    public FixturesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_fixtures, container, false);
+
+        return rootView;
+    }
+
+}
